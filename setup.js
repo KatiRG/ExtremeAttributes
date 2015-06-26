b25092ca6af83f10eaf7e90f205fb9d672f19c95
Cathy Nangini <cnangini@gmail.com>
2015-06-26 13:51:26 +0200

    Copied dc.treemap fn but missing some library

--- setup.js
+++ setup.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
         indexChart = dc.rowChart("#chart-indexType");
         yearChart = dc.barChart("#chart-eventYear");
         datasetChart = dc.rowChart("#chart-dataset");
+        indexTreeMap = dc.treemapChart("#treemap-index");
                 
         var colourRange = ["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"];        
 
@@ -512,3 +513,95 @@ function callHighChart(title) {
 
 
 }
+
+//treemap: https://github.com/dc-js/dc.js/issues/129
+dc.treemapChart = function (parent, chartGroup) {
+
+  var _chart = dc.baseMixin({});
+  var _treemap;
+
+  _chart._doRedraw = function() {
+    var _cellData = {
+      name:'tree',
+      children: _chart.data()
+    };
+    _chart.root()
+      .selectAll('.node')
+      .data(_treemap.nodes)
+      .transition().duration(1000)
+      .call(_updateCell);
+
+    _highlightFilters();
+    return _chart;
+  };
+
+  _chart._doRender = function () {
+
+    var color = d3.scale.category20c();
+
+    _treemap = d3.layout.treemap()
+      .size([_chart.width(), _chart.height()])
+      .sticky(true)
+      .value(function (d) { return d.value; });
+
+    var _cellData = {
+      name:'tree',
+      children: _chart.data()
+    };
+
+    _chart.root()
+      .classed('treemap', true)
+      .style('position', 'relative')
+      .style('height', _chart.height() + 'px')
+      .style('width', _chart.width() + 'px');
+
+    var _node = _chart.root()
+      .datum(_cellData)
+      .selectAll('.node')
+      .data(_treemap.nodes)
+      .enter()
+      .append('div')
+      .attr('class', 'node')
+      .call(_updateCell)
+      .style('background', function(d) { return color(d.key); })
+      .style('position', 'absolute')
+      .text(function(d) { return d.key; })
+      .attr('title', _chart.title())
+      .on('click', onClick);
+
+
+    return _chart;
+  };
+
+  function _updateCell() {
+    this.style('left', function (d) { return d.x + 'px'; })
+    .style('top', function (d) { return d.y + 'px'; })
+    .style('width', function (d) { return (d.dx - 1) + 'px'; })
+    .style('height', function (d) { return (d.dy - 1) + 'px'; })
+    .style('font-size', function (d) { return d.value > 0 ? 0.1 * Math.sqrt(d.dx * d.dy) +'px' : 0; });
+  }
+
+  function onClick(d, i) {
+    _chart.onClick(d, i);
+  }
+
+  function _highlightFilters() {
+    if (_chart.hasFilter()) {
+      _chart.root().selectAll('.node').each(function (d) {
+        if (_chart.hasFilter(d.key)) {
+          _chart.highlightSelected(this);
+        }
+        else {
+          _chart.fadeDeselected(this);
+        }
+      });
+    }
+    else {
+      _chart.root().selectAll('.node').each(function (d) {
+        _chart.resetHighlight(this);
+      });
+    }
+  }
+
+  return _chart.anchor(parent, chartGroup);
+};
